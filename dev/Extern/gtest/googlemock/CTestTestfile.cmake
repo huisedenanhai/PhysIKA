08f4f10559f2ec879c2aa1d8b8ee8d77812e7f0e
Weikm <18392889115@163.com>
2022-01-05 12:39:30 +0800

    remove dev

--- dev/Extern/gtest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/PhysIKA/Extern/gtest/googlemock
-# Build directory: D:/PhysIKA/dev/Extern/gtest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
